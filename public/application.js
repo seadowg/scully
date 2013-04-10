fc6181e9aa45cdf2d3633ef0f25b332273f6df53
Callum Stott <oetzi101@gmail.com>
2013-04-09 21:42:51 -0700

    Use Bacon.js for magicification :metal:

---
+++ public/application.js
@@ -0,0 +1,23 @@
+var formSubmits = function(el) {
+  var submits = new Bacon.Bus();
+  el.submit(function(ev, data) {
+    ev.preventDefault();
+    submits.plug(Bacon.fromPromise($.ajax({
+      type: "GET",
+      url: "/episodes/search",
+      dataType: "json",
+      data: $('#episode_input').serialize(),
+    })));
+  });
+  
+  return submits;
+};
+
+$(document).ready(function() {  
+  var submits = formSubmits($('#episode_form'));
+  var responses = submits.map(function(data) { return data['episode'] });
+  var keydowns = $('#episode_input').asEventStream('keydown');
+  responses.merge(keydowns.map("")).assign($("#next_episode"), "text");
+  
+  $('#episode_input').focus();
+});
