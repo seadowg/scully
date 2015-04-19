736e919b941c8d93c452adf3d02c324b0cf85621
Callum Stott <callum@seadowg.com>
2015-04-19 18:18:21 +0100

    Remove find_by_id helper

--- app/x_files.rb
+++ app/x_files.rb
@@ -4,7 +4,7 @@ require 'json'
 
 class XFiles < Sinatra::Base
   set :public_folder, 'public'
-  
+
   get'/' do
     File.read(File.join('public', 'index.html'))
   end
@@ -12,7 +12,7 @@ class XFiles < Sinatra::Base
   get '/episodes/search' do
     content_type :json
     episode = XFilesEpisode.find_by_name(params[:name])
-    
+
     if episode
       { :episode => episode['next'] }.to_json
     else
@@ -24,17 +24,5 @@ end
 class XFilesEpisode
   def self.find_by_name(name)
     Psych.load_file('./db/episodes.yml')[name.downcase]
-  end
-
-  def self.find_by_id(id)
-    result = Psych.load_file('./db/episodes.yml').select { |name, episode|
-      episode['id'] == id.to_i
-    }
-
-    if result.length > 0
-      result.first[1]
-    else
-      nil
-    end
   end
 end
